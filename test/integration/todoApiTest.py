88f058335a86d37d301e3ab2139876d86ba7cf44
Ubuntu <arenasmorante@gmail.com>
2022-05-16 16:49:00 +0000

    Modificacion en las pruebas de integracion

--- test/integration/todoApiTest.py
+++ test/integration/todoApiTest.py
@@ -232,6 +232,7 @@ class TestApi(unittest.TestCase):
             json_response['text'], "Integration text example - translate", "Error en la petición API a {url}"
         )
         #Delete TODO to restore state
+        url = BASE_URL+"/todos/"+ID_TODO
         response = requests.delete(url)
         self.assertEqual(
             response.status_code, 200, "Error en la petición API a {url}"
